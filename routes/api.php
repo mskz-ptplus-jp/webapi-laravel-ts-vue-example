1aab3132e5051c04ba642d1a3d68280f1d579785
Apache <msk_z@ptplus.jp>
2020-02-26 00:37:49 +0900

    WebAPI User

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['api'], 'prefix' => 'v1', 'namespace' => 'Api\V1'], function () {
+    Route::resource('users', 'UserController');
 });
