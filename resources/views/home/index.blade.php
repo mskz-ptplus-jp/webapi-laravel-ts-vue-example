8e6e65d762bc4567aa5e613dacc0b17636e3a0cd
Apache <msk_z@ptplus.jp>
2020-03-14 14:55:21 +0900

    Rename Index -> Home

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <script src="{{ asset(mix('js/app.js')) }}"></script>
+</head>
+<body>
+top.index.blade
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@
 |
 */
 
-Route::resource('/', 'TopController');
+Route::resource('/', 'HomeController');
