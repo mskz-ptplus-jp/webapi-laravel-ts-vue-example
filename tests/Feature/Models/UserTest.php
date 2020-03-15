34b72b0748cbbdfbf3d08ce79f3c46b49b31a594
Apache <msk_z@ptplus.jp>
2020-03-15 17:29:44 +0900

    Test Models\User Create

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -8,22 +8,14 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    protected $faker;
-    protected $model;
+    protected $faker = null;
+    protected $model = null;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->faker = \Faker\Factory::create('ja_JP');
-        $this->model = new \App\Models\User([
-            'name' => $this->faker->unique()->userName(),
-            'email' => $this->faker->unique()->email(),
-            'password' => bcrypt('1234'),
-            'email_verified_at' => $this->faker->dateTime(),
-            'created_at' => $this->faker->dateTime(),
-            'updated_at' => $this->faker->dateTime(),
-        ]);
     }
 
     /**
@@ -31,15 +23,15 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function Insert()
+    public function Create()
     {
-        $model = \App\Models\User::create([
-            'name' => $this->model->name,
-            'email' => $this->model->email,
-            'password' => $this->model->password,
-            'email_verified_at' => $this->model->email_verified_at,
-            'created_at' => $this->model->created_at,
-            'updated_at' => $this->model->updated_at,
+        $this->model = \App\Models\User::create([
+            'name' => $this->faker->unique()->userName(),
+            'email' => $this->faker->unique()->email(),
+            'password' => bcrypt('1234'),
+            'email_verified_at' => $this->faker->dateTime(),
+            'created_at' => $this->faker->dateTime(),
+            'updated_at' => $this->faker->dateTime(),
         ]);
         $this->assertDatabaseHas($this->model->getTable(), [
             'email' => $this->model->email
